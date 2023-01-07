5cb622fb9b3c1da099f305e50f4e80118fd04ec4
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-01-08 01:07:42 +0900

    fixing norm error

--- include/ray.h
+++ include/ray.h
@@ -6,7 +6,7 @@
 /*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 15:33:19 by hsano             #+#    #+#             */
-/*   Updated: 2023/01/04 15:15:02 by hsano            ###   ########.fr       */
+/*   Updated: 2023/01/08 01:01:12 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 # include "cub3d.h"
 # include <math.h>
 
-int	is_next_wall(t_cub3d *cub3d, t_ray *ray, t_cub3d_type angle);
 int	fire_ray(t_cub3d *cub3d, t_ray *ray, t_cub3d_type angle);
 #endif
