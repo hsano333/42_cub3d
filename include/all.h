322843b634cefbd2505a2d76c361b98045c86795
Masanori Aoyagi <maoyagi@c1r1s1.42tokyo.jp>
2023-01-14 23:39:25 +0900

    norm

--- include/all.h
+++ include/all.h
@@ -1,64 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_add_to_env.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maoyagi <maoyagi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 21:30:23 by maoyagi           #+#    #+#             */
+/*   Updated: 2023/01/14 23:38:55 by maoyagi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef ALL_H
-#define ALL_H
+# define ALL_H
 
-#include "cub3d.h"
-#include "cub3d_parser.h"
+# include "cub3d.h"
+# include "cub3d_parser.h"
 
-#include "../lib/libft/include/get_next_line.h"
-#include "../lib/libft/include/libft_isto.h"
-#include "../lib/libft/include/libft_mem.h"
-#include "../lib/libft/include/libft_put.h"
-#include "../lib/libft/include/libft_sort.h"
-#include "../lib/libft/include/libft_str.h"
-#include "../lib/libft/include/parse.h"
+# include "../lib/libft/include/get_next_line.h"
+# include "../lib/libft/include/libft_isto.h"
+# include "../lib/libft/include/libft_mem.h"
+# include "../lib/libft/include/libft_put.h"
+# include "../lib/libft/include/libft_sort.h"
+# include "../lib/libft/include/libft_str.h"
+# include "../lib/libft/include/parse.h"
 
-#include <stddef.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include "cub3d_parser.h"
+# include <stddef.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <errno.h>
+# include "cub3d_parser.h"
 
-#include <math.h>
-#include <stdbool.h>
-#include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <fcntl.h>
 
-bool ft_atoi_custom(long long *result, const char *str);
+bool	ft_atoi_custom(long long *result, const char *str);
 
 /************** exit	*****************/
-int exit_game(t_cub3d *env);
-void parse_map_exit(t_parser *parser);
-void add_to_env_exit(t_parser *parser, t_cub3d *env);
+int		exit_game(t_cub3d *env);
+void	parse_map_exit(t_parser *parser);
+void	add_to_env_exit(t_parser *parser, t_cub3d *env);
 
 /************** free	*****************/
-void *c3d_free(void *ptr);
-void free_array_of_char_pointers(char *array[]);
-void free_array_of_int_pointers(int *array[]);
-void free_pointers_from_char_array(char *array[]);
-void free_pointers_from_int_array(int *array[]);
+void	*c3d_free(void *ptr);
+void	free_array_of_char_pointers(char *array[]);
+void	free_array_of_int_pointers(int *array[]);
+void	free_pointers_from_char_array(char *array[]);
+void	free_pointers_from_int_array(int *array[]);
 
 /************** parsing	*****************/
-void parse_map_main(t_cub3d *env, char **argv);
-void print_parse_err_exit(t_parser *parser, t_cub3d *env);
-void print_file_err_exit(int err);
-int check_file(char **av, int *fd, t_parser *parser);
-int fill_parser_buf(t_parser *parser, int fd);
-int check_type(char *line, int *type);
-void parse_line(t_parser *parser, char *line);
-int add_in_err_buf(t_parser *parser, int error_type);
-int add_to_env(t_parser *parser, t_cub3d *env);
-int add_color_to_env(t_parser *parser, t_cub3d *env);
-int add_map_to_env(t_parser *parser, t_cub3d *env);
-int update_player_position(t_cub3d *env, char **map);
-int cpy_map_to_env(t_parser *parser, t_cub3d *env);
-char **freetab_index(char **tab, int index, int opt);
-int update_err_flag(t_parser *parser, int err_nbr);
-int add_info_err_buf(t_parser *parser, int type, int error_num);
-void putstr_err(const char *err_type, const char *err_message);
+void	parse_map_main(t_cub3d *env, char **argv);
+void	print_parse_err_exit(t_parser *parser, t_cub3d *env);
+void	print_file_err_exit(int err);
+int		check_file(char **av, int *fd, t_parser *parser);
+int		fill_parser_buf(t_parser *parser, int fd);
+int		check_type(char *line, int *type);
+void	parse_line(t_parser *parser, char *line);
+int		add_in_err_buf(t_parser *parser, int error_type);
+int		add_to_env(t_parser *parser, t_cub3d *env);
+int		add_color_to_env(t_parser *parser, t_cub3d *env);
+int		add_map_to_env(t_parser *parser, t_cub3d *env);
+int		update_player_position(t_cub3d *env, char **map);
+int		cpy_map_to_env(t_parser *parser, t_cub3d *env);
+char	**freetab_index(char **tab, int index, int opt);
+int		update_err_flag(t_parser *parser, int err_nbr);
+int		add_info_err_buf(t_parser *parser, int type, int error_num);
+void	putstr_err(const char *err_type, const char *err_message);
 
-void free_ptrarr(void **ptr);
-int check_player(t_parser *parser, t_cub3d *env);
-int check_cutted_map(t_parser *parser, t_cub3d *env);
+void	free_ptrarr(void **ptr);
+int		check_player(t_parser *parser, t_cub3d *env);
+int		check_cutted_map(t_parser *parser, t_cub3d *env);
 #endif
--- include/cub3d_parser.h
+++ include/cub3d_parser.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_add_to_env.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maoyagi <maoyagi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 21:30:23 by maoyagi           #+#    #+#             */
+/*   Updated: 2023/01/14 23:36:56 by maoyagi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef CUB3D_PARSER_H
 # define CUB3D_PARSER_H
 
@@ -19,118 +30,118 @@
 # define FOUND 0
 # define COLOR_MAX_LEN 11
 # define NBR_PARSING_ERR 11
-#define PARSER_BUFFER_SIZE 8192
-#define MAP_MAX_LENGH 8192
-#define MAP_MAX_HEIGHT 8192
-#define DIR 1.0
-#define CAM_PLANE 0.66
-#define X_ADJUST 0.5
-#define Y_ADJUST 0.5
-#define MAX_PATH_LENGH 800
+# define PARSER_BUFFER_SIZE 8192
+# define MAP_MAX_LENGH 8192
+# define MAP_MAX_HEIGHT 8192
+# define DIR 1.0
+# define CAM_PLANE 0.66
+# define X_ADJUST 0.5
+# define Y_ADJUST 0.5
+# define MAX_PATH_LENGH 800
 
-#define MS_MLX_INIT "1: mlx_init failed!\n"
-#define MS_MLX_RESOLUTION "2: the screen resolution is too low!\n"
-#define MS_MLX_NEW_IMG "3: mlx_new_image failed!\n"
-#define MS_MLX_NEW_WIN "4: mlx_new_window failed!\n"
+# define MS_MLX_INIT "1: mlx_init failed!\n"
+# define MS_MLX_RESOLUTION "2: the screen resolution is too low!\n"
+# define MS_MLX_NEW_IMG "3: mlx_new_image failed!\n"
+# define MS_MLX_NEW_WIN "4: mlx_new_window failed!\n"
 
-#define MS_BLK_ERR "blocking error"
-#define MS_NO_FILE "path to map is missing\n"
-#define MS_EXTENTION "invalid extention\n"
-#define MS_FOLDER "expecting : file.cut but received : directory.cub\n"
-#define MS_CHMOD "access denied\n"
-#define MS_PATH "path seems incorrect\n"
-#define MS_EMPTY_FILE "	: map seems to be empty\n"
-#define MS_TX_KEY_MISS "	: one or more texture path missing\n"
-#define MS_TX_PATH "	: texture path seems incorrect\n"
-#define MS_TX_PATH_LENGH "	: path too long\n"
-#define MS_FC_OVERFLOW "	: colors value cannot exceed 255 or wrong format\n"
-#define MS_FC_KEY_MISS "	: one or more color missing\n"
-#define MS_FC_FORM "	: expected color format : F/C 255,255,255\n"
-#define MS_MP_MISPLACED "	: map misplaced\n"
-#define MS_MP_TOO_SMALL "	: no map / map is too small\n"
-#define MS_MP_NO_PLAYER "	: a player is needed in the map\n"
-#define MS_MP_MULTI_PLAYER "	: found more than one player in the map\n"
-#define MS_MP_CUTTED "	: map cannot be cut\n"
-#define MS_MP_BORDERS "	: incorrect map borders\n"
-#define MS_MP_TOO_BIG "	: map is too big\n"
-#define MS_MP_UNABLE_TO_CPY "	: impossible to copy map\n"
-#define MS_GIBBER "	: found gibberish in infile\n"
-#define MS_FC_MULTIDEF "	: re-definition of colors ignored\n"
-#define MS_TX_MULTIDEF "	: re-definition of texture ignored\n"
+# define MS_BLK_ERR "blocking error"
+# define MS_NO_FILE "path to map is missing\n"
+# define MS_EXTENTION "invalid extention\n"
+# define MS_FOLDER "expecting : file.cut but received : directory.cub\n"
+# define MS_CHMOD "access denied\n"
+# define MS_PATH "path seems incorrect\n"
+# define MS_EMPTY_FILE "	: map seems to be empty\n"
+# define MS_TX_KEY_MISS "	: one or more texture path missing\n"
+# define MS_TX_PATH "	: texture path seems incorrect\n"
+# define MS_TX_PATH_LENGH "	: path too long\n"
+# define MS_FC_OVERFLOW "	: colors value cannot exceed 255 or wrong format\n"
+# define MS_FC_KEY_MISS "	: one or more color missing\n"
+# define MS_FC_FORM "	: expected color format : F/C 255,255,255\n"
+# define MS_MP_MISPLACED "	: map misplaced\n"
+# define MS_MP_TOO_SMALL "	: no map / map is too small\n"
+# define MS_MP_NO_PLAYER "	: a player is needed in the map\n"
+# define MS_MP_MULTI_PLAYER "	: found more than one player in the map\n"
+# define MS_MP_CUTTED "	: map cannot be cut\n"
+# define MS_MP_BORDERS "	: incorrect map borders\n"
+# define MS_MP_TOO_BIG "	: map is too big\n"
+# define MS_MP_UNABLE_TO_CPY "	: impossible to copy map\n"
+# define MS_GIBBER "	: found gibberish in infile\n"
+# define MS_FC_MULTIDEF "	: re-definition of colors ignored\n"
+# define MS_TX_MULTIDEF "	: re-definition of texture ignored\n"
 
-enum e_file_err
+enum	e_file_err
 {
-    ERR_NO_FILE,
-    ERR_EXTENTION,
-    ERR_FOLDER,
-    ERR_CHMOD,
-    ERR_PATH,
+	ERR_NO_FILE,
+	ERR_EXTENTION,
+	ERR_FOLDER,
+	ERR_CHMOD,
+	ERR_PATH,
 };
 
-enum e_map_err
+enum	e_map_err
 {
-    ERR_EMPTY_FILE,
-    ERR_TX_KEY_MISS,
-    ERR_TX_PATH,
-    ERR_TX_PATH_LENGH,
-    ERR_FC_OVERFLOW,
-    ERR_FC_KEY_MISS,
-    ERR_FC_FORM,
-    ERR_MP_MISPLACED,
-    ERR_MP_TOO_SMALL,
-    ERR_MP_NO_PLAYER,
-    ERR_MP_MULTI_PLAYER,
-    ERR_MP_CUTTED,
-    ERR_MP_BORDERS,
-    ERR_MP_TOO_BIG,
-    ERR_MP_UNABLE_TO_CPY,
-    ERR_GIBBER,
-    ERR_FC_MULTIDEF,
-    ERR_TX_MULTIDEF,
+	ERR_EMPTY_FILE,
+	ERR_TX_KEY_MISS,
+	ERR_TX_PATH,
+	ERR_TX_PATH_LENGH,
+	ERR_FC_OVERFLOW,
+	ERR_FC_KEY_MISS,
+	ERR_FC_FORM,
+	ERR_MP_MISPLACED,
+	ERR_MP_TOO_SMALL,
+	ERR_MP_NO_PLAYER,
+	ERR_MP_MULTI_PLAYER,
+	ERR_MP_CUTTED,
+	ERR_MP_BORDERS,
+	ERR_MP_TOO_BIG,
+	ERR_MP_UNABLE_TO_CPY,
+	ERR_GIBBER,
+	ERR_FC_MULTIDEF,
+	ERR_TX_MULTIDEF,
 };
 
-enum e_line_type
+enum	e_line_type
 {
-    TYPE_NO,
-    TYPE_SO,
-    TYPE_WE,
-    TYPE_EA,
-    TYPE_F,
-    TYPE_C,
-    TYPE_MAP,
-    TYPE_ERR,
-    TYPE_USELESS,
-    TYPE_EOF,
+	TYPE_NO,
+	TYPE_SO,
+	TYPE_WE,
+	TYPE_EA,
+	TYPE_F,
+	TYPE_C,
+	TYPE_MAP,
+	TYPE_ERR,
+	TYPE_USELESS,
+	TYPE_EOF,
 };
 
-enum e_err_buf_index
+enum	e_err_buf_index
 {
-    LINE_NBR,
-    ERROR_CODE,
+	LINE_NBR,
+	ERROR_CODE,
 };
 
-enum e_tab_free_opt
+enum	e_tab_free_opt
 {
-    NO_INDEX,
-    WITH_INDEX,
+	NO_INDEX,
+	WITH_INDEX,
 };
 
 typedef struct s_parser
 {
-    char *line;
-    int gnl_cnt;
-    int map_max_x;
-    int map_max_y;
-    int type;
-    char info_buf[6][MAX_PATH_LENGH];
-    int info_buf_line[6];
-    char *map_buf[PARSER_BUFFER_SIZE];
-    int map_line_buf[PARSER_BUFFER_SIZE];
-    int err_buf[PARSER_BUFFER_SIZE][2];
-    int info_buf_flag;
-    int map_buf_index;
-    int err_buf_index;
-    int blocking_err_flag;
-} t_parser;
+	char	*line;
+	int		gnl_cnt;
+	int		map_max_x;
+	int		map_max_y;
+	int		type;
+	char	info_buf[6][MAX_PATH_LENGH];
+	int		info_buf_line[6];
+	char	*map_buf[PARSER_BUFFER_SIZE];
+	int		map_line_buf[PARSER_BUFFER_SIZE];
+	int		err_buf[PARSER_BUFFER_SIZE][2];
+	int		info_buf_flag;
+	int		map_buf_index;
+	int		err_buf_index;
+	int		blocking_err_flag;
+}	t_parser;
 
 #endif
