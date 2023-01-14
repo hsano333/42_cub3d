fdd36ad95f14281bf504e7f4861838dc61fb17ca
Masanori Aoyagi <maoyagi@c1r1s1.42tokyo.jp>
2023-01-15 06:49:07 +0900

    norm

--- srcs/parser_add_info.c
+++ srcs/parser_add_info.c
@@ -6,7 +6,7 @@
 /*   By: maoyagi <maoyagi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 06:32:35 by maoyagi           #+#    #+#             */
-/*   Updated: 2023/01/15 06:45:26 by maoyagi          ###   ########.fr       */
+/*   Updated: 2023/01/15 06:48:44 by maoyagi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	add_info_err_buf(t_parser *parser, int type, int error_num)
 {
 	parser->blocking_err_flag |= (1 << error_num);
 	if (type <= TYPE_C)
-		parser->err_buf[parser->err_buf_index][LINE_NBR] = parser->info_buf_line[type];
+		parser->err_buf[parser->err_buf_index][LINE_NBR] \
+			= parser->info_buf_line[type];
 	else
 		parser->err_buf[parser->err_buf_index][LINE_NBR] = parser->gnl_cnt;
 	parser->err_buf[parser->err_buf_index][ERROR_CODE] = error_num;
