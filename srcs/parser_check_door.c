95a94f7c6a503706502587a29bac716157376ea0
Masanori Aoyagi <maoyagi@c1r1s1.42tokyo.jp>
2023-01-15 14:44:07 +0900

    check door

--- srcs/parser_check_door.c
+++ srcs/parser_check_door.c
@@ -6,41 +6,48 @@
 /*   By: maoyagi <maoyagi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 12:56:29 by maoyagi           #+#    #+#             */
-/*   Updated: 2023/01/15 12:56:29 by maoyagi          ###   ########.fr       */
+/*   Updated: 2023/01/15 14:43:53 by maoyagi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/all.h"
 #include "../include/cub3d_parser.h"
 
-bool is_ok_door(t_parser *parser, size_t y, size_t x)
+bool	is_ok_door(t_parser *parser, size_t y, size_t x)
 {
-    return (!(((parser->map_buf[y - 1][x] == '1' && parser->map_buf[y + 1][x] == '1') && (parser->map_buf[y][x - 1] == '0' && parser->map_buf[y][x + 1] == '0')) || ((parser->map_buf[y - 1][x] == '0' && parser->map_buf[y + 1][x] == '0') && (parser->map_buf[y][x - 1] == '1' && parser->map_buf[y][x + 1] == '1'))));
+	return (!(((parser->map_buf[y - 1][x] == '1' && \
+			parser->map_buf[y + 1][x] == '1') && \
+			(parser->map_buf[y][x - 1] == '0' && \
+			parser->map_buf[y][x + 1] == '0')) \
+			|| ((parser->map_buf[y - 1][x] == '0' \
+			&& parser->map_buf[y + 1][x] == '0') \
+			&& (parser->map_buf[y][x - 1] == '1' \
+			&& parser->map_buf[y][x + 1] == '1'))));
 }
 
-bool check_door(t_parser *parser)
+bool	check_door(t_parser *parser)
 {
-    size_t y;
-    size_t x;
-    bool err_flag;
+	size_t	y;
+	size_t	x;
+	bool	err_flag;
 
-    err_flag = true;
-    y = 0;
-    while (parser->map_buf[y])
-    {
-        x = 0;
-        while (parser->map_buf[y][x] != '\n')
-        {
-            if (parser->map_buf[y][x] == 'D')
-            {
-                if (is_ok_door(parser, y, x))
-                    err_flag = false;
-            }
-            x++;
-        }
-        y++;
-    }
-    if (!err_flag)
-        return (update_err_flag(parser, ERR_GIBBER));
-    return (false);
+	err_flag = true;
+	y = 0;
+	while (parser->map_buf[y])
+	{
+		x = 0;
+		while (parser->map_buf[y][x] != '\n')
+		{
+			if (parser->map_buf[y][x] == 'D')
+			{
+				if (is_ok_door(parser, y, x))
+					err_flag = false;
+			}
+			x++;
+		}
+		y++;
+	}
+	if (!err_flag)
+		return (update_err_flag(parser, ERR_GIBBER));
+	return (false);
 }
